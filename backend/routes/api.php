ec7d7b825f3218b54440344335712df44473a10a
Mustafa Yılmaz <mustafa.yilmaz@aartimuhendislik.com>
2024-01-31 10:28:07 +0300

    Controllers created and routes added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transanction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +20,31 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+/**
+ * Buyers
+ */
+Route::resource('buyers', BuyerController::class );
+/**
+ * Sellers
+ */
+Route::resource('sellers', SellerController::class);
+/**
+ * Products
+ */
+Route::resource('products', ProductController::class);
+/**
+ * Categories
+ */
+Route::resource('categories', CategoryController::class);
+/**
+ * Transactions
+ */
+Route::resource('transactions', TransactionController::class);
+/**
+ * Users
+ */
+Route::resource('user', UserController::class);
+
 
 
